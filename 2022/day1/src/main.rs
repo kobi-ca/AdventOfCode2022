5aae5db03c046fda20e983df66fac7c5f75cb650
Yacob (Kobi) Cohen-Arazi <kobic@qti.qualcomm.com>
2022-12-03 21:44:38 -0800

    finished day1 part2 as well
    reminder - day2 was done with rust group in Q

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -3,7 +3,7 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 
-fn main() {
+fn part1() {
     let mut max = 0;
     if let Ok(lines) = read_lines("./input.txt") {
         let mut count: u64 = 0;
@@ -18,6 +18,30 @@ fn main() {
         }
     }
     println!("{}", max);
+}
+
+fn part2() {
+    let mut v = vec![];
+    if let Ok(lines) = read_lines("./input.txt") {
+        let mut count: u64 = 0;
+        for line in lines {
+            let ip = line.unwrap();
+            if ip.is_empty() {
+                v.push(count);
+                count = 0;
+            } else {
+                count += ip.parse::<u64>().unwrap();
+            }
+        }
+    }
+    v.sort_by(|a, b| b.cmp(a));
+    let result: u64 = v.iter().take(3).sum();
+    println!("{}", result);
+}
+
+fn main() {
+    part1();
+    part2();
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
