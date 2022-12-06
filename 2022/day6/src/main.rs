b78e9b4b57e8226158684554db0fe885ccbe49f9
Yacob (Kobi) Cohen-Arazi <kobic@qti.qualcomm.com>
2022-12-06 12:39:01 -0800

    Introduce WINDOW_SIZE as const
    can be changed to any window size

--- 2022/day6/src/main.rs
+++ 2022/day6/src/main.rs
@@ -5,16 +5,17 @@ use iterwindows::IterArrayWindows;
 use itertools::Itertools;
 
 fn main() {
+    const WINDOW_SIZE: usize = 14; // 4 for part1, 14 for part2
     let lines = read_lines("input.txt").unwrap();
     // in this case, it's a single line
     let s = lines.into_iter().next().unwrap().unwrap();
     //let r = s.chars().next_chunk();
-    let r = s.chars().array_windows::<4>();
-    let mut counter = 4;
+    let r = s.chars().array_windows::<WINDOW_SIZE>();
+    let mut counter = WINDOW_SIZE; // 4 for part1
     for chunk in r {
         //println!("{:?}", chunk);
         let res = chunk.to_vec().into_iter().unique().collect::<Vec<_>>();
-        if res.len() ==4 {
+        if res.len() == WINDOW_SIZE {
             break;
         }
         counter += 1;
