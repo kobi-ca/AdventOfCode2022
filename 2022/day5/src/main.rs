30c8203c85a55d5721fdbbf898bc780b87e5a5ef
Yacob (Kobi) Cohen-Arazi <kobic@qti.qualcomm.com>
2022-12-06 10:30:20 -0800

    day5 part2 completed
    using rev iter to extend an existing one

--- 2022/day5/src/main.rs
+++ 2022/day5/src/main.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use std::num;
 use std::path::Path;
 extern crate regex;
 
@@ -34,8 +33,7 @@ use regex::Regex;
 // for i in 0..=num_stacks {
 //     let stack_id: char = line.iter().skip(1).
 
-fn main() {
-
+fn part1() {
     let mut v: Vec<Vec<char>> = vec![
         vec!['F', 'T', 'C', 'L', 'R', 'P', 'G', 'Q'],
         vec!['N', 'Q', 'H', 'W', 'R', 'F', 'S', 'J'],
@@ -85,6 +83,65 @@ fn main() {
                 v[7].last().unwrap(),
                 v[8].last().unwrap()
             );
+}
+
+fn part2() {
+    let mut v: Vec<Vec<char>> = vec![
+        vec!['F', 'T', 'C', 'L', 'R', 'P', 'G', 'Q'],
+        vec!['N', 'Q', 'H', 'W', 'R', 'F', 'S', 'J'],
+        vec!['F', 'B', 'H', 'W', 'P', 'M', 'Q'],
+        vec!['V', 'S', 'T', 'D', 'F'],
+        vec!['Q', 'L', 'D', 'W', 'V', 'F', 'Z'],
+        vec!['Z', 'C', 'L', 'S'],
+        vec!['Z', 'B', 'M', 'V', 'D', 'F'],
+        vec!['T', 'J', 'B'],
+        vec!['Q', 'N', 'B', 'G', 'L', 'S', 'P', 'H'],
+    ];
+
+    let lines = read_lines("input.txt").unwrap();
+    let mut skip = 0;
+    let input_re = Regex::new(
+        r#"move (\d*) from (\d*) to (\d*)"#
+    ).unwrap();
+    for line in lines {
+        if skip < 10 {
+            skip +=1;
+            continue;
+        }
+        let l = line.unwrap();
+        let cap = input_re.captures(l.as_str()).unwrap();
+        // println!("{:?}", cap.get(1).map_or("???", |m| m.as_str()));
+        let mut num_elems_to_move = cap.get(1)
+            .map(|m| m.as_str().parse::<usize>().unwrap() ).unwrap();
+        let from = cap.get(2)
+            .map(|m| m.as_str().parse::<usize>().unwrap() ).unwrap();
+        let to = cap.get(3)
+            .map(|m| m.as_str().parse::<usize>().unwrap() ).unwrap();
+
+        let mut tmp_vec: Vec<char> = vec![];
+        while num_elems_to_move > 0 {
+            let elem = v[from - 1].pop().unwrap();
+            tmp_vec.push(elem);
+            num_elems_to_move -= 1;
+        }
+        v[to - 1].extend(tmp_vec.iter().rev());
+    }
+    println!("{}{}{}{}{}{}{}{}{}",
+                v[0].last().unwrap(),
+                v[1].last().unwrap(),
+                v[2].last().unwrap(),
+                v[3].last().unwrap(),
+                v[4].last().unwrap(),
+                v[5].last().unwrap(),
+                v[6].last().unwrap(),
+                v[7].last().unwrap(),
+                v[8].last().unwrap()
+            );
+}
+
+fn main() {
+    part1();
+    part2();
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
